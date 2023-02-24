411ac0735b9988d22992210384d0a4b45ddbfc07
clarence <clarencejames@live.com.au>
2023-02-24 21:57:10 +1100

    wip db design and function of cards

--- metrics-dashboard-app/src/App.js
+++ metrics-dashboard-app/src/App.js
@@ -23,19 +23,38 @@ function App() {
 
   // }, []);
 
-  const [ICParticipatedAndClosed, setICParticipatedAndClosed] = useState([]);
-  const ICParticipatedAndClosedCollectionRef = collection(db, "ICParticipatedAndClosed")
-  console.log(ICParticipatedAndClosed)
+  // const [ICParticipatedAndClosed, setICParticipatedAndClosed] = useState([]);
+  // const ICParticipatedAndClosedCollectionRef = collection(db, "ICParticipatedAndClosed")
+  // console.log(ICParticipatedAndClosed)
+
+  // useEffect(() => {
+  //   const getICParticipatedAndClosed = async () => {
+  //     const data = await getDocs(ICParticipatedAndClosedCollectionRef);
+  //     console.log(data)
+  //     setICParticipatedAndClosed(data.docs.map(doc => ({...doc.data(), id: doc.id})))
+  //   }
+  //   getICParticipatedAndClosed();
+
+  // }, []);
+
+  const [numberOfOnboardings, setNumberOfOnboardings] = useState([]);
+  const numberOfOnboardingsCollectionRef = collection(db, "numberOfOnboardings")
+  // console.log(numberOfOnboardings[0].onboardingRemaining)
 
   useEffect(() => {
-    const getICParticipatedAndClosed = async () => {
-      const data = await getDocs(ICParticipatedAndClosedCollectionRef);
+    const getNumberOfOnboardings = async () => {
+      const data = await getDocs(numberOfOnboardingsCollectionRef);
       // console.log(data)
-      setICParticipatedAndClosed(data.docs.map(doc => ({...doc.data(), id: doc.id})))
+      setNumberOfOnboardings(data.docs.map(doc => ({...doc.data(), id: doc.id})))
     }
-    getICParticipatedAndClosed();
+    getNumberOfOnboardings();
 
   }, []);
+
+  let totalOnboardings = numberOfOnboardings.reduce((acc, curr) => {
+    return acc + curr.onboardingRemaining
+  }, 0)
+  // console.log(totalOnboardings)
 
   return (
     <div className="App">
@@ -44,7 +63,7 @@ function App() {
       <div className="dashboard-container">
         <Header className="header"/>
         <div className="card-container">
-          <MetricCard  ICParticipatedAndClosed={ICParticipatedAndClosed}/>
+          <MetricCard  totalOnboardings={totalOnboardings} />
           <MetricCard  />
           <MetricCard  />
           <MetricCard  />
@@ -57,3 +76,11 @@ function App() {
 }
 
 export default App;
+
+// Questions: 
+// Data - 
+// 1. Are my collections/documents structured correctly?
+
+// Layout - 
+// 1. Is how im passing data correct? Parent -> Child
+// 2. When I click on a card, i want to remove all cards and show one large card with data.
--- metrics-dashboard-app/src/MetricCard.js
+++ metrics-dashboard-app/src/MetricCard.js
@@ -1,18 +1,23 @@
 // import Card from '@mui/material/Card';
 import './MetricCard.css';
 
+
 // TO DO:
 // The metric card is a component that will always remain the same
 // create a sub component for the card contents
 
-export default function MetricCard( {ICParticipatedAndClosed} ) {
-    console.log(ICParticipatedAndClosed)
-    return (
-        <div className="metric-card-container">
-            <div className="card-contents">
-                <h1>IC Participated and Closed</h1>
-                <h2>{ICParticipatedAndClosed}</h2>
+export default function MetricCard(props) {
+    console.log(props)
 
+    const { active, onClick } = props;
+
+
+    return (
+        <div className={`metric-card ${active ? 'active' : ''}`} onClick={onClick}>
+            <div className="metric-card-container">
+                <div className="card-contents">
+                    <h1>{props.totalOnboardings}</h1>
+                </div>
             </div>
         </div>
     )
